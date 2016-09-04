254950adbd1b3cf0cbd985af164cfa4a7d11f35d
timeff <kawin.ac125@gmail.com>
2016-09-04 13:59:31 +0000

    add landing page

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ router.get("/",function(req,res){
     res.render("main",{header: "Let's find the tutor"});
 })
 
+router.get("/landing",function(req,res){
+    res.render("landing",{header: "Let's find the tutor"});
+})
 
 module.exports=router;
